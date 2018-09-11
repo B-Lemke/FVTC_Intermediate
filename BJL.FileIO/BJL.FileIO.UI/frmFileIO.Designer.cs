6e1e37bc1b23ae7053df6825d4e3e9d57dca51a6
Broderick Lemke <500161066@fvtc.edu>
2018-09-11 12:08:54 -0500

    Adding File IO Read and Write

---
+++ BJL.FileIO/BJL.FileIO.UI/frmFileIO.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace BJL.FileIO.UI
+{
+    partial class frmFileIO
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWriteText = new System.Windows.Forms.Button();
+            this.btnReadAll = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnWriteText
+            // 
+            this.btnWriteText.Location = new System.Drawing.Point(12, 12);
+            this.btnWriteText.Name = "btnWriteText";
+            this.btnWriteText.Size = new System.Drawing.Size(148, 23);
+            this.btnWriteText.TabIndex = 0;
+            this.btnWriteText.Text = "&Write";
+            this.btnWriteText.UseVisualStyleBackColor = true;
+            this.btnWriteText.Click += new System.EventHandler(this.btnWriteText_Click);
+            // 
+            // btnReadAll
+            // 
+            this.btnReadAll.Location = new System.Drawing.Point(12, 43);
+            this.btnReadAll.Name = "btnReadAll";
+            this.btnReadAll.Size = new System.Drawing.Size(148, 23);
+            this.btnReadAll.TabIndex = 1;
+            this.btnReadAll.Text = "Read &All";
+            this.btnReadAll.UseVisualStyleBackColor = true;
+            this.btnReadAll.Click += new System.EventHandler(this.btnReadAll_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 74);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 105);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(148, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 136);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(148, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 167);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(148, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(212, 15);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(527, 135);
+            this.txtInput.TabIndex = 6;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOutput.Location = new System.Drawing.Point(212, 167);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(527, 174);
+            this.lblOutput.TabIndex = 7;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 408);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(803, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 17);
+            // 
+            // frmFileIO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 430);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnReadAll);
+            this.Controls.Add(this.btnWriteText);
+            this.Name = "frmFileIO";
+            this.Text = "File IO Application";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWriteText;
+        private System.Windows.Forms.Button btnReadAll;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+    }
+}
+
---
+++ BJL.FileIO/BJL.FileIO.UI/frmFileIO.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BJL.FileIO.UI
+{
+    public partial class frmFileIO : Form
+    {
+        public frmFileIO()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWriteText_Click(object sender, EventArgs e)
+        {
+            lblStatus.Text = string.Empty;
+
+            string fileName = @"c:\Users\public\data.txt";
+
+            try
+            {
+                //write out the contents of the textbox to a flat file
+
+                StreamWriter sw;
+
+                //Hook the writer to the file
+                sw = File.CreateText(fileName);
+
+                //put the data in the file
+                sw.WriteLine(txtInput.Text);
+
+                // Cleanup
+                sw.Close();
+
+                sw = null;
+
+                lblStatus.ForeColor = Color.Green;
+                lblStatus.Text = "Data Written to File: " + fileName;
+            }
+            catch (Exception ex)
+            {
+                lblStatus.ForeColor = Color.Red;
+                lblStatus.Text = ex.Message;
+            }
+        }
+
+        private void btnReadAll_Click(object sender, EventArgs e)
+        {
+            lblStatus.Text = string.Empty;
+
+            string fileName = "c:\\users\\public\\data.txt";
+
+            try
+            {
+                StreamReader sr;
+
+                // check to see if file exists
+                if (File.Exists(fileName))
+                {
+                    sr = File.OpenText(fileName);
+
+                    //get file contents
+                    lblOutput.Text = sr.ReadToEnd();
+
+                    //Cleanup
+                    sr.Close();
+                    sr = null;
+
+                    lblStatus.ForeColor = Color.Green;
+                    lblStatus.Text = "File Read:  " + fileName;
+                }
+                else
+                {
+                    throw new Exception("404: File not found - " + fileName);
+                }
+                
+
+
+
+            }
+            catch (Exception ex)
+            {
+                lblStatus.ForeColor = Color.Red;
+                lblStatus.Text = ex.Message;
+            }
+        }
+
+
+    }
+}
