e9a72ae8a1229957041cbfa38608ea11ad832790
Broderick Lemke <500161066@fvtc.edu>
2018-09-18 12:16:46 -0500

    Added array functionality and speech synthesis

--- BJL.Arrays/BJL.Arrays.Ui/frmArrays.Designer.cs
+++ BJL.Arrays/BJL.Arrays.Ui/frmArrays.Designer.cs
@@ -28,20 +28,236 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCreateScores = new System.Windows.Forms.Button();
+            this.btnCreateScoresLoop = new System.Windows.Forms.Button();
+            this.btnCreateNames = new System.Windows.Forms.Button();
+            this.btnDisplayNames = new System.Windows.Forms.Button();
+            this.btnSpeak = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnSortDesc = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnResize = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.lbxOutput = new System.Windows.Forms.ListBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // btnCreateScores
+            // 
+            this.btnCreateScores.Location = new System.Drawing.Point(12, 12);
+            this.btnCreateScores.Name = "btnCreateScores";
+            this.btnCreateScores.Size = new System.Drawing.Size(154, 23);
+            this.btnCreateScores.TabIndex = 0;
+            this.btnCreateScores.Text = "Create Scores (1D)";
+            this.btnCreateScores.UseVisualStyleBackColor = true;
+            this.btnCreateScores.Click += new System.EventHandler(this.btnCreateScores_Click);
+            // 
+            // btnCreateScoresLoop
+            // 
+            this.btnCreateScoresLoop.Location = new System.Drawing.Point(12, 51);
+            this.btnCreateScoresLoop.Name = "btnCreateScoresLoop";
+            this.btnCreateScoresLoop.Size = new System.Drawing.Size(154, 23);
+            this.btnCreateScoresLoop.TabIndex = 1;
+            this.btnCreateScoresLoop.Text = "Create Scores (1D-2)";
+            this.btnCreateScoresLoop.UseVisualStyleBackColor = true;
+            this.btnCreateScoresLoop.Click += new System.EventHandler(this.btnCreateScoresLoop_Click);
+            // 
+            // btnCreateNames
+            // 
+            this.btnCreateNames.Location = new System.Drawing.Point(12, 90);
+            this.btnCreateNames.Name = "btnCreateNames";
+            this.btnCreateNames.Size = new System.Drawing.Size(154, 23);
+            this.btnCreateNames.TabIndex = 2;
+            this.btnCreateNames.Text = "Create Names";
+            this.btnCreateNames.UseVisualStyleBackColor = true;
+            this.btnCreateNames.Click += new System.EventHandler(this.btnCreateNames_Click);
+            // 
+            // btnDisplayNames
+            // 
+            this.btnDisplayNames.Enabled = false;
+            this.btnDisplayNames.Location = new System.Drawing.Point(12, 129);
+            this.btnDisplayNames.Name = "btnDisplayNames";
+            this.btnDisplayNames.Size = new System.Drawing.Size(154, 23);
+            this.btnDisplayNames.TabIndex = 3;
+            this.btnDisplayNames.Text = "Display Names";
+            this.btnDisplayNames.UseVisualStyleBackColor = true;
+            this.btnDisplayNames.Click += new System.EventHandler(this.btnDisplayNames_Click);
+            // 
+            // btnSpeak
+            // 
+            this.btnSpeak.Location = new System.Drawing.Point(12, 168);
+            this.btnSpeak.Name = "btnSpeak";
+            this.btnSpeak.Size = new System.Drawing.Size(154, 23);
+            this.btnSpeak.TabIndex = 4;
+            this.btnSpeak.Text = "Speak";
+            this.btnSpeak.UseVisualStyleBackColor = true;
+            this.btnSpeak.Click += new System.EventHandler(this.btnSpeak_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Enabled = false;
+            this.btnSort.Location = new System.Drawing.Point(12, 207);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(154, 23);
+            this.btnSort.TabIndex = 5;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // btnSortDesc
+            // 
+            this.btnSortDesc.Enabled = false;
+            this.btnSortDesc.Location = new System.Drawing.Point(12, 246);
+            this.btnSortDesc.Name = "btnSortDesc";
+            this.btnSortDesc.Size = new System.Drawing.Size(154, 23);
+            this.btnSortDesc.TabIndex = 6;
+            this.btnSortDesc.Text = "Sort Descending";
+            this.btnSortDesc.UseVisualStyleBackColor = true;
+            this.btnSortDesc.Click += new System.EventHandler(this.btnSortDesc_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(12, 285);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(154, 23);
+            this.btnPlay.TabIndex = 7;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnResize
+            // 
+            this.btnResize.Location = new System.Drawing.Point(12, 324);
+            this.btnResize.Name = "btnResize";
+            this.btnResize.Size = new System.Drawing.Size(154, 23);
+            this.btnResize.TabIndex = 8;
+            this.btnResize.Text = "Resize";
+            this.btnResize.UseVisualStyleBackColor = true;
+            this.btnResize.Click += new System.EventHandler(this.btnResize_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(12, 363);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(154, 23);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(12, 402);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(154, 23);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "button11";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(12, 441);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(154, 23);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "button12";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(12, 480);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(154, 23);
+            this.button13.TabIndex = 12;
+            this.button13.Text = "button13";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(12, 519);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(154, 23);
+            this.button14.TabIndex = 13;
+            this.button14.Text = "button14";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(12, 558);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(154, 23);
+            this.button15.TabIndex = 14;
+            this.button15.Text = "button15";
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // lbxOutput
+            // 
+            this.lbxOutput.FormattingEnabled = true;
+            this.lbxOutput.Location = new System.Drawing.Point(184, 12);
+            this.lbxOutput.Name = "lbxOutput";
+            this.lbxOutput.Size = new System.Drawing.Size(337, 433);
+            this.lbxOutput.TabIndex = 15;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(528, 12);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(319, 433);
+            this.txtInput.TabIndex = 17;
             // 
             // frmArrays
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(873, 617);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lbxOutput);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.btnResize);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnSortDesc);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnSpeak);
+            this.Controls.Add(this.btnDisplayNames);
+            this.Controls.Add(this.btnCreateNames);
+            this.Controls.Add(this.btnCreateScoresLoop);
+            this.Controls.Add(this.btnCreateScores);
             this.Name = "frmArrays";
             this.Text = "Arrays";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCreateScores;
+        private System.Windows.Forms.Button btnCreateScoresLoop;
+        private System.Windows.Forms.Button btnCreateNames;
+        private System.Windows.Forms.Button btnDisplayNames;
+        private System.Windows.Forms.Button btnSpeak;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnSortDesc;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnResize;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.ListBox lbxOutput;
+        private System.Windows.Forms.TextBox txtInput;
     }
 }
 
--- BJL.Arrays/BJL.Arrays.Ui/frmArrays.cs
+++ BJL.Arrays/BJL.Arrays.Ui/frmArrays.cs
@@ -7,14 +7,148 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech.Synthesis;
 
 namespace BJL.Arrays.Ui
 {
     public partial class frmArrays : Form
     {
+        string[] names;
+
         public frmArrays()
         {
             InitializeComponent();
+        }
+
+        private void btnCreateScores_Click(object sender, EventArgs e)
+        {
+
+
+            //declare and initialize an array
+            double[] scores = { 33.5, 44.7, 55.3 };
+
+
+            //Add array elements to the lbxOutput
+            displayContents(scores);
+
+
+        }
+
+        private void btnCreateScoresLoop_Click(object sender, EventArgs e)
+        {
+
+
+            //declare and initialize an empty array of size 3
+            double[] scores = new double[3];
+            scores[0] = 33.5;
+            scores[1] = 44.7;
+            scores[2] = 55.3;
+
+
+            //display
+            displayContents(scores);
+        }
+
+        private void btnCreateNames_Click(object sender, EventArgs e)
+        {
+            //NO BUENO : names = {"Homer", "Marge", "Bart", "Lisa", "Maggie"};
+            names = new string[5];
+            names[0] = "Homer";
+            names[1] = "Marge";
+            names[2] = "Bart";
+            names[3] = "Lisa";
+            names[4] = "Maggie";
+
+            btnDisplayNames.Enabled = true;
+            btnSort.Enabled = true;
+            btnSortDesc.Enabled = true;
+        }
+
+        private void btnDisplayNames_Click(object sender, EventArgs e)
+        {
+            displayContents(names);
+        }
+
+        private void displayContents(double[] info)
+        {
+            lbxOutput.Items.Clear();
+
+            //loop through the doubles and print to lbxOutput.
+            for (int i = 0; i < info.Length; i++)
+            {
+                lbxOutput.Items.Add(info[i].ToString());
+            }
+        }
+
+        private void displayContents(string[] info)
+        {
+            lbxOutput.Items.Clear();
+
+            //loop through the strings and print to lbxOutput.
+            foreach(string s in info)
+            {
+                lbxOutput.Items.Add(s);
+            }
+
+        }
+
+        private void displayContents(int[] info)
+        {
+            lbxOutput.Items.Clear();
+
+            //loop through the ints and print to lbxOutput.
+            for (int i = 0; i < info.Length; i++)
+            {
+                lbxOutput.Items.Add(info[i].ToString());
+            }
+
+        }
+
+        private void btnSpeak_Click(object sender, EventArgs e)
+        {
+            SpeechSynthesizer speaker = new SpeechSynthesizer();
+
+            speaker.Rate = 5;
+            speaker.Volume = 100;
+
+            speaker.SetOutputToWaveFile(@"output.wav");
+
+            speaker.Speak(txtInput.Text);
+
+            speaker = null;
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            //Sort an array
+            Array.Sort(names);
+
+            displayContents(names);
+        }
+
+        private void btnSortDesc_Click(object sender, EventArgs e)
+        {
+            //Sort an array
+            Array.Sort(names);
+            Array.Reverse(names);
+
+            displayContents(names);
+
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+
+            player.SoundLocation = @"output.wav";
+            player.Play();
+        }
+
+        private void btnResize_Click(object sender, EventArgs e)
+        {
+            Array.Resize(ref names, names.Length + 1);
+            names[names.Length - 1] = "Flanders";
+            displayContents(names);
         }
     }
 }
