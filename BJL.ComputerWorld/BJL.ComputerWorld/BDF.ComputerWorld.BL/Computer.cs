5639e9ce820710a7e89688bfe2773f82898a87c1
Broderick Lemke <500161066@fvtc.edu>
2018-10-18 12:18:57 -0500

    Added UI And BL Code

---
+++ BJL.ComputerWorld/BJL.ComputerWorld/BDF.ComputerWorld.BL/Computer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDF.ComputerWorld.BL
+{
+    public class Computer : Equipment
+    {
+        private int hardDriveSize;
+
+        public int HardDriveSize
+        {
+            get { return hardDriveSize; }
+            set { hardDriveSize = value; }
+        }
+
+        private int ram;
+
+        public int RAM
+        {
+            get { return ram; }
+            set { ram = value; }
+        }
+
+
+    }
+
+    public class ComputerList : List<Computer>
+    {
+
+        /// <summary>
+        /// Hard code a list of computers
+        /// </summary>
+        public void Seed()
+        {
+            Computer computer1;
+            computer1 = new Computer();
+
+            //Set the properties
+            computer1.Id = 1;
+            computer1.Manufacturer = "HP";
+            computer1.Model = "6100";
+            computer1.Price = 499.99;
+            computer1.RAM = 16;
+            computer1.HardDriveSize = 1024;
+            computer1.SerialNo = "xxx-xx-xx1a";
+            computer1.Description = "My shiny new computer";
+            computer1.EquipmentType = 2;
+
+            this.Add(computer1);
+
+            Computer computer2 = new Computer();
+            //Set the properties
+            computer2.Id = 2;
+            computer2.Manufacturer = "Dell";
+            computer2.Model = "3000";
+            computer2.Price = 109.99;
+            computer2.RAM = 4;
+            computer2.HardDriveSize = 256;
+            computer2.SerialNo = "xxx-xx-xx2b";
+            computer2.Description = "Crappy small computer";
+            computer2.EquipmentType = 3;
+
+            this.Add(computer2);
+        }
+    }
+}
---
+++ BJL.ComputerWorld/BJL.ComputerWorld/BDF.ComputerWorld.UI/frmComputerWorld.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BDF.ComputerWorld.BL;
+
+namespace BDF.ComputerWorld.UI
+{
+    public partial class frmComputerWorld : Form
+    {
+        //Store computers in a modular variable that is a ComputerList
+        ComputerList computerList;
+
+
+        public frmComputerWorld()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadComputers_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                computerList = new ComputerList();
+
+                computerList.Seed();
+
+                //bind the data to the screen
+                dgvComputers.DataSource = null;
+                dgvComputers.DataSource = computerList;
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = ex.Message;
+                lblStatus.ForeColor = Color.DarkRed;
+            }
+        }
+
+        private void dgvComputers_SelectionChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
